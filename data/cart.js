f848f94936c7be185e69faeabd1a7870079075be
harikrishnan5058 <alphahari5058@gmail.com>
2025-06-14 17:41:03 +0530

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,7 @@
-export let cart = [
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+  cart = [
   {
     productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity : 2
@@ -8,6 +11,14 @@ export let cart = [
     quantity : 2
   },
 ];
+
+}
+
+function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+
 
 export function addToCart(productId){
 
@@ -27,6 +38,7 @@ let matchingItem;
         quantity: 1
       });
       }
+      saveToStorage()
 
 }
 
@@ -40,4 +52,5 @@ export function removeFromCart(productId){
     });
 
     cart = newCart;
+    saveToStorage()
 }
